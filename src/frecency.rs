ba26a1983a1dcbe5f744126d0b0e6e058907bd0f
Euan Kemp <euank@euank.com>
2017-09-17 23:33:51 -0700

    frecency: simplify 'min' finding

--- src/frecency.rs
+++ src/frecency.rs
@@ -50,25 +50,7 @@ impl<T> Frecency<T> where T: Hash, T: Eq, T: Ord, T: Clone {
 
     fn trim_min(&mut self) {
         let min_key = {
-            let mut min_entry = None;
-            for e in &self.frecency {
-                min_entry = match min_entry {
-                    None => {
-                        Some(e)
-                    },
-                    Some(old_min) => {
-                        if old_min.1 > e.1 {
-                            Some(e)
-                        } else {
-                            Some(old_min)
-                        }
-                    }
-                };
-            };
-            match min_entry{
-                None => None,
-                Some(e) => Some(e.0.clone()),
-            }
+            self.items().last().map(|e| e.clone())
         };
 
         if let Some(min) = min_key {
