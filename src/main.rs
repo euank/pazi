f5e704b294157fe3bf487e048d9030282901b6a6
Euan Kemp <euan.kemp@coreos.com>
2017-11-29 21:32:56 -0800

    init: don't cd to '--help'
    Make an exception for `z --help`

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,7 @@ add-zsh-hook chpwd __pazi_add_dir
 
 pazi_cd() {
     [ "$#" -eq 0 ] && pazi && return 0
+    [[ "$@[(r)--help]" == "--help" ]] && pazi --help && return 0
     local to=$(pazi --dir "$@")
     [ -z "${to}" ] && return 1
     cd "${to}"
