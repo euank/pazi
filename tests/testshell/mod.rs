2680ff623ebd7f88f8e50f0b53017441fcc965bc
Euan Kemp <euan.kemp@coreos.com>
2017-12-11 20:04:03 -0800

    integ: add a 'testshell' vte driver
    This driver is necessary for testing bash because bash, even compared to
    'bash -i', behaves differently when under a pty.
    
    We need the bash behaviour of running PROMPT_COMMAND in order to test
    pazi correctly, and I actually can't think of a better way to deal with
    bash's garbage interactive mode other than this: creating a full pty and
    parsing the vte output.
    
    It's possible that there's a better solution out there, but honestly
    this is attractive for other reasons too.
    Benchmarking in this environment will be much more "real world".
    Sure, the cost was my sanity since working with pty's is actual hell,
    but I think what I've got here more or less works.

---
+++ tests/testshell/mod.rs
@@ -0,0 +1,189 @@
+extern crate pty;
+extern crate vte;
+
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+use std::io::Read;
+use std::io::Write;
+use std::time::Duration;
+use std::sync::mpsc;
+use std::thread;
+
+pub struct TestShell {
+    // fork is here for lifetime reasons; can't drop it until the pty is done
+    fork: pty::fork::Fork,
+    pty: pty::fork::Master,
+    output: mpsc::Receiver<String>,
+    eof: mpsc::Receiver<()>,
+}
+
+// VTEData is to handle lines after the mess of vte terminal stuff.
+// It keeps track of newlines and such
+#[derive(PartialEq, Clone, Debug)]
+struct VTEData {
+    pub current_line: String,
+    pub scrollback: Vec<String>,
+}
+
+impl VTEData {
+    fn new() -> Self {
+        VTEData{
+            current_line: String::new(),
+            scrollback: Vec::new(),
+        }
+    }
+}
+
+impl vte::Perform for VTEData {
+    fn print(&mut self, c: char) {
+        self.current_line.push(c);
+    }
+
+    fn execute(&mut self, byte: u8) {
+        if byte as char == '\n' {
+            self.scrollback.push(self.current_line.clone());
+            self.current_line = String::new();
+        }
+    }
+
+    fn hook(&mut self, params: &[i64], intermediates: &[u8], ignore: bool) {
+        // ignore
+    }
+
+    fn put(&mut self, byte: u8) {
+        // ignore
+    }
+
+    fn unhook(&mut self) {
+        // ignore
+    }
+
+    fn osc_dispatch(&mut self, params: &[&[u8]]) {
+        // ignore
+    }
+
+    fn csi_dispatch(&mut self, params: &[i64], intermediates: &[u8], ignore: bool, c: char) {
+        // ignore
+    }
+
+    fn esc_dispatch(&mut self, params: &[i64], intermediates: &[u8], ignore: bool, byte: u8) {
+        // ignore
+    }
+}
+
+impl TestShell {
+    // TODO pattern instead of regex once that's stable
+    // new creates a new testshell. It is assumed that the passed in command is for a posix-ish
+    // shell. The shell should print output generally line-by-line and after executing a command,
+    // it should print the PS1 variable.
+    // This PS1 variable is used to determine when commands have executed, so no commands run in
+    // this testshell may print the PS1 value.
+    // Note: this command does fork off a child. There are dragons. Handle with care.
+    pub fn new(mut cmd: Command, ps1: &str) -> Self {
+        cmd.env("PS1", ps1);
+        let fork = pty::fork::Fork::from_ptmx().unwrap();
+
+        let mut pty = match fork {
+            pty::fork::Fork::Child(_) => {
+                let err = cmd.exec();
+                panic!("exec failed: {}", err);
+            }
+            pty::fork::Fork::Parent(_, m) => {
+                m.grantpt().unwrap();
+                m.unlockpt().unwrap();
+                m
+            }
+        };
+
+        let (write_command_out, command_out) = mpsc::channel();
+        let (write_eof_got, eof_got) = mpsc::channel();
+
+        // To move into the thread
+        let ps12 = ps1.to_owned();
+        thread::spawn(move || {
+            // vte stuff
+            let mut data = VTEData::new();
+            let mut statemachine = vte::Parser::new();
+            // Keep a copy of the vte so we know when things have changed vs, e.g., a character
+            // just being a control sequence.
+            let mut last_data = data.clone();
+
+            // Have we seen the starting PS1 yet?
+            let mut initial_prompt = false;
+            // What's been output since the last PS1 + command happened?
+            let mut current_command_output = Vec::new();
+            loop {
+                let mut buf: [u8; 4 * 1024] = [0; 4 * 1024];
+                let nread = pty.read(&mut buf).unwrap();
+                if nread == 0 {
+                    write_eof_got.send(()).unwrap();
+                    // EOF
+                    return;
+                }
+                for byte in &buf[..nread] {
+                    statemachine.advance(&mut data, *byte);
+                    if last_data == data {
+                        // control character or whatever, we don't care
+                        continue;
+                    }
+                    // More characters have been pushed on, let's see what we have
+
+                    if data.current_line == ps12 {
+                        // Exactly equal to PS1 means that there's a new blank PS1 prompt
+                        // Either we just started up, or a command just finished.
+                        if !initial_prompt {
+                            // not startup, sometihng finished
+                            write_command_out.send(current_command_output.join("\n")).unwrap();
+                            current_command_output = Vec::new();
+                        }
+                        initial_prompt = false;
+                    } else if data.scrollback.len() > last_data.scrollback.len() {
+                        // this only happens if the last character was a newline since we're
+                        // checking this every statemachine advance.
+                        let last_line = data.scrollback.last().unwrap();
+                        // skip PS1 starting things since we assume that's a command being entered,
+                        // e.g....
+                        //     PS1 $ ls
+                        //     file1 file2
+                        //     PS1 $
+                        // We're avoiding the first line there
+                        if !last_line.starts_with(&ps12) {
+                            current_command_output.push(last_line.to_string());
+                        }
+                    }
+                    last_data = data.clone();
+                }
+            }
+        });
+
+        // Receive an initial "empty string" thing I tried to skip above but failed to :shrug:
+        command_out.recv().unwrap();
+
+        TestShell{
+            fork: fork,
+            pty: pty,
+            eof: eof_got,
+            output: command_out,
+        }
+    }
+
+    pub fn run(&mut self, cmd: &str) -> String {
+        self.pty.write(format!("{}\n", cmd).as_bytes()).unwrap();
+        self.output.recv_timeout(Duration::from_secs(1)).unwrap()
+    }
+
+    pub fn shutdown(&mut self) {
+        self.pty.write("exit\n".as_bytes()).unwrap();
+        self.eof.recv().unwrap();
+    }
+}
+
+#[test]
+fn testshell() {
+    let cmd = Command::new("bash");
+    let mut ts = TestShell::new(cmd, "==> ");
+    assert_eq!(ts.run("echo hi"), "hi");
+    assert_eq!(ts.run("echo foo"), "foo");
+    assert_eq!(ts.run(r#"echo -e "foo\nbar\nbaz" | tac"#), "baz\nbar\nfoo");
+    ts.shutdown();
+}
