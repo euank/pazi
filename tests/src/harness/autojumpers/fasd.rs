ffb7a20b9bb255341d47e6fe0db7bc60436b9d3d
Euan Kemp <euank@euank.com>
2017-12-15 18:12:49 -0800

    tests/harness/fasd: update to use submodule

--- tests/src/harness/autojumpers/fasd.rs
+++ tests/src/harness/autojumpers/fasd.rs
@@ -1,35 +1,19 @@
 use super::Autojumper;
 use harness::Shell;
 use std::env;
-use std::process::Command;
 use std::path::Path;
-use std::fs;
-use std::os::unix::fs::PermissionsExt;
 
 pub struct Fasd;
-
-const FASD_VERSION: &str = "90b531a";
 
 impl Autojumper for Fasd {
     fn bin_path(&self) -> String {
         let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("build with cargo");
         let fasd_path = Path::new(&crate_dir)
-            .join(format!(".cache/fasd/{}/fasd", FASD_VERSION));
+            .join(format!("testbins/fasd/fasd"));
 
-        if fasd_path.exists() {
-            return fasd_path.canonicalize().unwrap().to_string_lossy().to_string();
+        if !fasd_path.exists() {
+            panic!("update submodules before running benches");
         }
-        // doesn't exist, let's create it..
-        let dir = fasd_path.parent().expect("could not get parent");
-        fs::create_dir_all(dir).expect("could not create dir");
-        println!("2");
-        let url = format!("https://raw.githubusercontent.com/clvv/fasd/{}/fasd", FASD_VERSION);
-        Command::new("curl")
-            .args(vec!["-s", "-o", &fasd_path.to_string_lossy(), &url])
-            .status().unwrap();
-
-        fs::set_permissions(&fasd_path, fs::Permissions::from_mode(0o755)).unwrap();
-
         fasd_path.canonicalize().unwrap().to_string_lossy().to_string()
     }
 
