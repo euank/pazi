4244bbaf9520a2f3d85981d05269d013450b4d0e
Euan Kemp <euank@euank.com>
2017-12-15 16:39:35 -0800

    benches: hide behind a nightly feature

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,8 +1,10 @@
-#![feature(test)]
+#![cfg_attr(feature = "nightly", feature(test))]
 
 extern crate pty;
 extern crate tempdir;
 
 mod harness;
 mod integ;
+
+#[cfg(feature = "nightly")]
 mod bench;
