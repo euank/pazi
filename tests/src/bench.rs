81ebe3517f94ab8f82b8213822713acaa18a7de0
Euan Kemp <euank@euank.com>
2017-12-16 16:56:08 -0800

    benches: add a cd bench
    Due to pazi and fasd operating in significantly different ways, namely
    on zsh using a hook vs parsing prompt input, and on bash vs checking
    only prompt PWD vs parsing all args, this test is probably a more
    interesting one.
    
    People typically do not do highly '&&'d commands. However, in the cd_50
    bench, pazi's method of working is invoked once per '&&' while fasd is
    invoked once for the whole thing.
    
    This difference does matter, so I'm leaving the cd_50 in, but this is a
    more useful comparison for regular use.

--- tests/src/bench.rs
+++ tests/src/bench.rs
@@ -5,6 +5,33 @@ use tempdir::TempDir;
 use harness::{Harness, Autojumper, Shell, Pazi, Fasd, NoJumper};
 use self::test::Bencher;
 
+fn cd_bench(b: &mut Bencher, jumper: &Autojumper, shell: &Shell) {
+    let tmpdir = TempDir::new("pazi_bench").unwrap();
+    let root = tmpdir.path();
+    let mut h = Harness::new(&root, jumper, shell);
+    let dir1p = root.join("tmp1");
+    let dir2p = root.join("tmp2");
+    let dir1 = dir1p.to_str().unwrap();
+    let dir2 = dir1p.to_str().unwrap();
+
+    h.create_dir(&dir1);
+    h.create_dir(&dir2);
+
+    // ensure we hit different directories on adjacent iterations; autojumpers may validly avoid
+    // doing work on 'cd .'.
+    let mut iter = 0;
+    b.iter(move || {
+        let dir = if iter % 2 == 0 {
+            &dir1
+        } else {
+            &dir2
+        };
+        iter += 1;
+        h.visit_dir(dir)
+    });
+}
+
+
 fn cd_50_bench(b: &mut Bencher, jumper: &Autojumper, shell: &Shell) {
     let tmpdir = TempDir::new("pazi_bench").unwrap();
     let root = tmpdir.path();
@@ -24,6 +51,7 @@ fn cd_50_bench(b: &mut Bencher, jumper: &Autojumper, shell: &Shell) {
         h.run_cmd(&cmd)
     });
 }
+
 
 #[bench]
 fn it_cds_to_50_directories_none_zsh(b: &mut Bencher) {
@@ -54,4 +82,35 @@ fn it_cds_to_50_directories_fasd_zsh(b: &mut Bencher) {
 #[bench]
 fn it_cds_to_50_directories_fasd_bash(b: &mut Bencher) {
     cd_50_bench(b, &Fasd, &Shell::Bash);
+}
+
+#[bench]
+fn it_cds_to_2_directories_none_zsh(b: &mut Bencher) {
+    cd_bench(b, &NoJumper, &Shell::Zsh);
+}
+
+#[bench]
+fn it_cds_to_2_directories_none_bash(b: &mut Bencher) {
+    cd_bench(b, &NoJumper, &Shell::Bash);
+}
+
+#[bench]
+fn it_cds_to_2_directories_pazi_zsh(b: &mut Bencher) {
+    cd_bench(b, &Pazi, &Shell::Zsh);
+}
+
+#[bench]
+fn it_cds_to_2_directories_pazi_bash(b: &mut Bencher) {
+    cd_bench(b, &Pazi, &Shell::Bash);
+}
+
+
+#[bench]
+fn it_cds_to_2_directories_fasd_zsh(b: &mut Bencher) {
+    cd_bench(b, &Fasd, &Shell::Zsh);
+}
+
+#[bench]
+fn it_cds_to_2_directories_fasd_bash(b: &mut Bencher) {
+    cd_bench(b, &Fasd, &Shell::Bash);
 }
